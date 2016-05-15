81e084d885c87ffa5c3e5445d193d8df61e7dcfd
tsv <todor.stoyanov@oru.se>
2016-05-15 14:44:07 +0200

    gripper scripts

---
+++ demo_grasping/scripts/closeGrippers.sh
@@ -0,0 +1,3 @@
+rosservice call /yumi/yumi_gripper/do_grasp 'gripper_id: 1' 
+sleep 1
+rosservice call /yumi/yumi_gripper/do_grasp 'gripper_id: 2' 
---
+++ demo_grasping/scripts/openGrippers.sh
@@ -0,0 +1,3 @@
+rosservice call /yumi/yumi_gripper/release_grasp 'gripper_id: 1' 
+sleep 1
+rosservice call /yumi/yumi_gripper/release_grasp 'gripper_id: 2' 
