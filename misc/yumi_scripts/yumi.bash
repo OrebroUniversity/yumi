ea32500f57f75331d6b76e960f6b65b16ec7c1c0
FWachter <wachterfreddy@gmail.com>
2016-06-03 17:03:56 +0200

    Fixed sourcing on setupWS files
    Sourcing was not done from the home directory, added in sourcing from home directory

--- misc/yumi_scripts/yumi.bash
+++ misc/yumi_scripts/yumi.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 # PROGRAMMER: Frederick Wachter
 # DATE CREATED: 2016-05-20
-# LAST MODIFIED: 2016-05-24
 # PURPOSE: Make it easier to run YuMi scripts - Run YuMi in RViz
 
-source yumi_ws/devel/setup.bash # source the catkin workspace
+source ~/yumi_ws/devel/setup.bash # source the catkin workspace
 roslaunch yumi_moveit_config moveit_planning_execution.launch # launch YuMi in RViz
 
 
--- misc/yumi_scripts/yumi_server.bash
+++ misc/yumi_scripts/yumi_server.bash
@@ -1,16 +1,14 @@
 #!/bin/bash
 # PROGRAMMER: Frederick Wachter
 # DATE CREATED: 2016-05-20
-# LAST MODIFIED: 2016-05-24
 # PURPOSE: Make it easier to run YuMi scripts - Run server to send trajectories to YuMi
-# NOTES: 10.37.129.22
 
 if [ -z "$1" ]; then
 	echo "Please input the robot ip as an argument"
 	exit
 fi
 
-source yumi_ws/devel/setup.bash # source the catkin workspace
+source ~/yumi_ws/devel/setup.bash # source the catkin workspace
 roslaunch yumi_support robot_interface.launch robot_ip:=$1 # run YuMi server to send files to real YuMi robot at the specified IP address
 
 
