915deeee7c331097da4eaf83ba69d8414f8b28ca
YoshuaNava <yoshua.nava.chocron@gmail.com>
2017-05-31 18:47:47 +0200

    Adding a new sub-package, yumi_test_controllers, to test the controllers and functions of the robot.

---
+++ yumi_test_controllers/include/yumi_test_controllers.h
@@ -0,0 +1,21 @@
+#ifndef __YUMI_TEST_CONTROLLERS_H
+#define __YUMI_TEST_CONTROLLERS_H
+
+
+#include <cstdlib>
+#include <iostream>
+
+#include <boost/thread/mutex.hpp>
+#include <boost/thread.hpp>
+
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <sensor_msgs/JointState.h>
+#include <trajectory_msgs/JointTrajectory.h>
+
+using namespace std;
+
+
+
+
+#endif
