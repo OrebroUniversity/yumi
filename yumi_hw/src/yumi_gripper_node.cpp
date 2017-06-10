1601831e2d1c492097220f3c5eacabebc76e4897
YoshuaNava <yoshua.nava.chocron@gmail.com>
2017-06-10 22:34:01 +0200

    Changed the grippers interface to EffortJointInterface in the URDF. Deleted all grippers rws file. Updated the general launch files

--- yumi_hw/src/yumi_gripper_node.cpp
+++ yumi_hw/src/yumi_gripper_node.cpp
@@ -3,6 +3,10 @@
 
 #include <yumi_hw/yumi_gripper_node.h>
 
+
+/*! Main method of the yumi_rws_gripper_node file. (testing doxygen)
+ *
+ */
 int main( int argc, char* argv[] )
 {
     ros::init(argc, argv, "yumi_gripper_node");
