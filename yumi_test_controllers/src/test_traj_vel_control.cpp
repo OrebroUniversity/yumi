8bdcf2581088972312e3cb164bae11ef01b7faee
YoshuaNava <yoshua.nava.chocron@gmail.com>
2017-07-17 17:22:18 +0200

    Fixing an error in one of the test_controllers script

--- yumi_test_controllers/src/test_traj_vel_control.cpp
+++ yumi_test_controllers/src/test_traj_vel_control.cpp
@@ -38,28 +38,29 @@ void right_vel_controller_callback(const ros::SingleSubscriberPublisher& pub)
 
 void joint_states_callback(const sensor_msgs::JointState &msg)
 {
-	ROS_INFO("Joint states update!");
+	// ROS_INFO("Joint states update!");
 	joints_state.name = msg.name;
 	joints_state.position = msg.position;
 
 
 	if(!left_command_sent)
 	{
+		ROS_INFO("Left arm command!");
 		trajectory_msgs::JointTrajectoryPoint p;
-		for (int i = 0; i < num_joints_arm; i++)
+		for (int i = 0; i < num_joints; i+=2)
 		{
 	        p.positions.push_back(joints_state.position[i]);
-	        p.velocities.push_back(0.0);
-	        p.accelerations.push_back(0.0);
+	        // p.velocities.push_back(0.0);
+	        // p.accelerations.push_back(0.0);
 	        p.time_from_start = ros::Duration(1.0);
 		}
 
-		int joint_test_idx = 4;
-		p.positions[joint_test_idx] = p.positions[joint_test_idx] - 0.2;
-		p.velocities[joint_test_idx] = -1.0;
+		int joint_test_idx = 2;
+		p.positions[joint_test_idx] = p.positions[joint_test_idx] - 0.5;
+		// p.velocities[joint_test_idx] = -1.0;
 
 		left_traj.points.push_back(p);
-		left_traj.header.stamp = ros::Time::now();
+		left_traj.header.stamp = ros::Time(0.0);
 
 		cout << "Publishing command" << endl;
 		left_pub.publish(left_traj);
@@ -69,21 +70,22 @@ void joint_states_callback(const sensor_msgs::JointState &msg)
 
 	if(!right_command_sent)
 	{
+		ROS_INFO("Right arm command!");
 		trajectory_msgs::JointTrajectoryPoint p;
-		for (int i = 0; i < num_joints_arm; i++)
+		for (int i = 1; i < num_joints; i+=2)
 		{
 	        p.positions.push_back(joints_state.position[i]);
-	        p.velocities.push_back(0.0);
-	        p.accelerations.push_back(0.0);
+	        // p.velocities.push_back(0.0);
+	        // p.accelerations.push_back(0.0);
 	        p.time_from_start = ros::Duration(1.0);
 		}
 
 		int joint_test_idx = 4;
-		p.positions[joint_test_idx] = p.positions[joint_test_idx] - 0.2;
-		p.velocities[joint_test_idx] = -1.0;
+		p.positions[joint_test_idx] = p.positions[joint_test_idx] - 0.5;
+		// p.velocities[joint_test_idx] = -1.0;
 
 		right_traj.points.push_back(p);
-		right_traj.header.stamp = ros::Time::now();
+		right_traj.header.stamp = ros::Time(0.0);
 
 		cout << "Publishing command" << endl;
 		right_pub.publish(right_traj);
@@ -106,19 +108,19 @@ int main( int argc, char* argv[] )
 
     left_traj.joint_names.push_back("yumi_joint_1_l");
     left_traj.joint_names.push_back("yumi_joint_2_l");
-    left_traj.joint_names.push_back("yumi_joint_7_l");
     left_traj.joint_names.push_back("yumi_joint_3_l");
     left_traj.joint_names.push_back("yumi_joint_4_l");
     left_traj.joint_names.push_back("yumi_joint_5_l");
     left_traj.joint_names.push_back("yumi_joint_6_l");
+	left_traj.joint_names.push_back("yumi_joint_7_l");
     
 	right_traj.joint_names.push_back("yumi_joint_1_r");
 	right_traj.joint_names.push_back("yumi_joint_2_r");
-	right_traj.joint_names.push_back("yumi_joint_7_r");
 	right_traj.joint_names.push_back("yumi_joint_3_r");
 	right_traj.joint_names.push_back("yumi_joint_4_r");
 	right_traj.joint_names.push_back("yumi_joint_5_r");
 	right_traj.joint_names.push_back("yumi_joint_6_r");
+	right_traj.joint_names.push_back("yumi_joint_7_r");
 
 
     sub = nh.subscribe("/yumi/joint_states", 1000, joint_states_callback);
